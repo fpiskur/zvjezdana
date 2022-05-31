f9998d0d169d953532b8f9f792970fda1b07ba9c
fpiskur <filip.piskur@gmail.com>
2022-05-31 21:38:24 +0200

    List clients, basic layout.

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,6 @@
 class ClientsController < ApplicationController
+
+  def index
+    @clients = Client.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   resources :clients
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "application#hello"
+  root 'clients#index'
 end
