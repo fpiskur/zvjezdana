6a5235add13196f03c2ed3d715fa3537850854e7
fpiskur <filip.piskur@gmail.com>
2022-06-02 21:30:39 +0200

    Add client show action, basic styling. Add client list pagination

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,6 +1,10 @@
 class ClientsController < ApplicationController
 
   def index
-    @clients = Client.all
+    @clients = Client.all.paginate(page: params[:page], per_page: 50)
+  end
+
+  def show
+    @client = Client.find(params[:id])
   end
 end
