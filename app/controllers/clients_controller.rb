289ac92602375d0012bee221eedb0e507a81ca83
fpiskur <filip.piskur@gmail.com>
2022-06-04 16:27:12 +0200

    Enable adding new clients

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,9 +2,27 @@ class ClientsController < ApplicationController
 
   def index
     @clients = Client.all.paginate(page: params[:page], per_page: 50)
+    @client = Client.new
   end
 
   def show
     @client = Client.find(params[:id])
   end
+
+  def create
+    @client = Client.new(client_params)
+    if @client.save
+      flash[:success] = "Novi korisnik je dodan!"
+      redirect_to client_path(@client)
+    else
+      puts "Nije se sejvao klijent"
+    end
+  end
+
+  private
+
+    def client_params
+      params.require(:client).permit(:first_name, :last_name, :phone_num,
+                                  :comment)
+    end
 end
