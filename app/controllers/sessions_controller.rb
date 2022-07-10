def6c65e90bab4a2b5839ca18f9a9ae9d220bf23
fpiskur <filip.piskur@gmail.com>
2022-07-10 20:47:09 +0200

    Add create and destroy actions in sessions controller, fix new session form bug (usernmae instead of username)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
+
   def new
+  end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      # Log the user in and redirect to the user's show page.
+    else
+      flash.now[:danger] = "Korisničko ime i/ili lozinka nisu ispravni!"
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
   end
 end
