17ee82445ca082415a3cdc69f5e25d43c409cc75
fpiskur <filip.piskur@gmail.com>
2022-07-24 13:59:15 +0200

    Remember user on login and forget on logout

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,14 +4,35 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   def logged_in?
-    if session[:user_id]
-      user = User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      user = User.find_by(id: user_id)
       user == User.find_by(username: "zvjezdana")
+    elsif (user_id = cookies.encrypted[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        user == User.find_by(username: "zvjezdana")
+      end
     end
   end
 
+  # Forgets a persistent session
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   def log_out
+    user = User.find_by(username: "zvjezdana")
+    forget(user)
     reset_session
   end
 end
