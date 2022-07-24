34022c55442a8255fafc3eec7f6df6a7da7f71af
fpiskur <filip.piskur@gmail.com>
2022-07-24 11:40:18 +0200

    Create migration to add remember_digest to users, create remember method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
 
+  attr_accessor :remember_token
+
   has_secure_password
 
   validates :username, presence: true
@@ -9,6 +11,16 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Remembers a user in the database for use in persistent sessions
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_07_171312) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_24_092536) do
   create_table "clients", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_07_171312) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_digest"
   end
 
   add_foreign_key "treatments", "clients"
