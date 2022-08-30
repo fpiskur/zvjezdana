07f931d63d7620ae61a7321a3e78fb4704b6b618
fpiskur <filip.piskur@gmail.com>
2022-08-30 16:56:30 +0200

    Add user login intergration tests for unsuccessful login

--- test/integration/user_login_test.rb
+++ test/integration/user_login_test.rb
@@ -10,6 +10,7 @@ class UserLoginTest < ActionDispatch::IntegrationTest
   test "login as admin with valid username/password" do
     get login_path
     assert_template 'sessions/new'
+    assert_select 'form#login-form'
     post login_path, params: { session: { username: @admin_user.username,
                                           password: 'zvjezdana' } }
     assert is_logged_in?
@@ -20,5 +21,33 @@ class UserLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", logout_path
     assert_select 'h2', text: 'Novi klijent'
     assert_select 'form#add-client'
+  end
+
+  test "login as non-admin user" do
+    get login_path
+    post login_path, params: { session: { username: @other_user.username,
+                                          password: 'other' } }
+    refute is_logged_in?
+    assert_nil cookies[:remember_token]
+    refute flash.empty?
+    assert_template 'sessions/new'
+    assert_select 'form#login-form'
+  end
+
+  test "login with valid admin username/invalid password" do
+    get login_path
+    post login_path, params: { session: { username: @admin_user.username,
+                                          password: 'password' } }
+    refute is_logged_in?
+    refute flash.empty?
+    assert_template 'sessions/new'
+  end
+
+  test "login with invalid information" do
+    get login_path
+    post login_path, params: { session: { username: '', password: '' } }
+    refute is_logged_in?
+    refute flash.empty?
+    assert_template 'sessions/new'
   end
 end
