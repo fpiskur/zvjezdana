8ef7722a3a6b7bf158cd7fb61d2bd8ae425309bd
fpiskur <filip.piskur@gmail.com>
2022-08-31 18:21:23 +0200

    Add treatment model tests

--- test/models/treatment_test.rb
+++ test/models/treatment_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
 class TreatmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @client = clients(:one)
+    @treatment = @client.treatments.build(date: "2022-05-05",
+                                          description: "blablabla")
+  end
+
+  test "should be valid" do
+    assert @treatment.valid?
+  end
+
+  test "client id should be present" do
+    @treatment.client_id = nil
+    refute @treatment.valid?
+  end
+
+  test "date should be present" do
+    @treatment.date = "     "
+    refute @treatment.valid?
+  end
+
+  test "description should be present" do
+    @treatment.description = "     "
+    refute @treatment.valid?
+  end
+
 end
