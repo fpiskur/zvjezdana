997fcda36200a4543d8b2b03498278871b4ae075
fpiskur <filip.piskur@gmail.com>
2022-09-10 18:44:45 +0200

    Split integration tests for clients_add_test into clients_add_test & clients_edit_test

--- test/integration/clients_add_test.rb
+++ test/integration/clients_add_test.rb
@@ -41,30 +41,4 @@ class ClientsAddTest < ActionDispatch::IntegrationTest
     assert_select 'form#add-treatment'
   end
 
-  test "edit client with invalid information" do
-    log_in_as(@user)
-    patch client_path(@client), params: { client: { first_name: "  ",
-                                                         last_name: "  ",
-                                                         phone_num: "",
-                                                         comment: "" } }
-    assert_template 'edit'
-    refute flash.empty?
-    assert_select 'div.alert-danger', text: "Greška prilikom uređivanja "\
-              "klijenta! - Ispunite barem jedno od polja - Ime ili Prezime."
-    refute @client.reload.first_name.empty?
-  end
-
-  test "edit client with valid information" do
-    log_in_as(@user)
-    patch client_path(@client), params: { client: { first_name: "Test",
-                                                         last_name: "  ",
-                                                         phone_num: "",
-                                                         comment: "" } }
-    assert_redirected_to client_path(@client)
-    follow_redirect!
-    refute flash.empty?
-    assert_select 'div.alert-success', text: "Izmjene su spremljene!"
-    assert_equal "Test", @client.reload.first_name
-  end
-
 end
