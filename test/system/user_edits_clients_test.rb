7b7be52a86ae6cd3f42c8347439d17eb51b45781
fpiskur <filip.piskur@gmail.com>
2022-09-11 12:48:43 +0200

    Split user_opens_clients_test to opens_client and edits_client tests

---
+++ test/system/user_edits_clients_test.rb
@@ -0,0 +1,28 @@
+require "application_system_test_case"
+
+class UserEditsClientsTest < ApplicationSystemTestCase
+  
+  def setup
+    @client = clients(:two)
+    log_in_admin
+  end
+
+  test "edit client" do
+    first("a[href='#{client_path(@client.id)}']").click
+    assert_selector "a.edit-client-btn"
+    click_link("uredi klijenta")
+    assert_selector "form#edit-client"
+    assert_equal @client.first_name,
+                 find("input[name='client[first_name]']").value
+    assert_equal @client.last_name,
+                 find("input[name='client[last_name]']").value
+    assert_equal @client.phone_num,
+                 find("input[name='client[phone_num]']").value
+    assert_equal @client.comment,
+                 find("textarea[name='client[comment]']").value
+    assert_selector "button.back-btn"
+    click_button('NATRAG')
+    assert_current_path(client_path(@client))
+  end
+
+end
--- test/system/user_opens_clients_test.rb
+++ test/system/user_opens_clients_test.rb
@@ -20,22 +20,4 @@ class UserOpensClientsTest < ApplicationSystemTestCase
           first("ol.treatments li .treatment-info .treatment-date")['innerText']
   end
 
-  test "edit client" do
-    first("a[href='#{client_path(@client.id)}']").click
-    assert_selector "a.edit-client-btn"
-    click_link("uredi klijenta")
-    assert_selector "form#edit-client"
-    assert_equal @client.first_name,
-                 find("input[name='client[first_name]']").value
-    assert_equal @client.last_name,
-                 find("input[name='client[last_name]']").value
-    assert_equal @client.phone_num,
-                 find("input[name='client[phone_num]']").value
-    assert_equal @client.comment,
-                 find("textarea[name='client[comment]']").value
-    assert_selector "button.back-btn"
-    click_button('NATRAG')
-    assert_current_path(client_path(@client))
-  end
-
 end
